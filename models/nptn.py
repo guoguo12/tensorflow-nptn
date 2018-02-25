ccfe3dc328e28d0869b637674cbc396404ca3d26
Allen Guo <guoguo12@gmail.com>
2018-02-25 00:01:47 -0800

    Rewrite model from paper using TensorFlow

--- models/nptn.py
+++ models/nptn.py
@@ -4,6 +4,7 @@ from keras.layers.advanced_activations import PReLU
 from keras.models import Sequential
 import tensorflow as tf
 
+
 def nptn_layer(input, kernel_size, in_channels, out_channels, num_transforms):
     """NPTN layer
     use separate scopes to use multiple NPTN layers
@@ -44,27 +45,35 @@ def nptn_layer(input, kernel_size, in_channels, out_channels, num_transforms):
     return output + bias
 
 
-def two_layer_nptn(input_shape, num_classes, out_channels, num_transforms):
+def two_layer_nptn(x, num_classes, out_channels, num_transforms):
     """Two-layer NPTN as described in the paper.
 
     out_channels and num_transforms refer to the first layer only.
     """
-    model = Sequential()
+    with tf.variable_scope('nptn_1'):
+        x = nptn_layer(x, 3, 3, out_channels, num_transforms)
+        x = tf.layers.batch_normalization(x)
+        x = parametric_relu(x)
+        x = tf.layers.max_pooling2d(x, (2, 2), 2)
 
-    layer_1 = lambda x: nptn_layer(x, 3, 3, out_channels, num_transforms)
-    layer_2 = lambda x: nptn_layer(x, 3, out_channels, 16, 1)
+    with tf.variable_scope('nptn_2'):
+        x = nptn_layer(x, 3, out_channels, 16, 1)
+        x = tf.layers.batch_normalization(x)
+        x = parametric_relu(x)
+        x = tf.layers.max_pooling2d(x, (2, 2), 2)
 
-    model.add(Lambda(layer_1, input_shape=input_shape))
-    model.add(BatchNormalization())
-    model.add(PReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    x = tf.contrib.layers.flatten(x)
+    logits = tf.layers.dense(x, units=num_classes)
 
-    model.add(Lambda(layer_2))
-    model.add(BatchNormalization())
-    model.add(PReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    return logits
 
-    model.add(Flatten())
-    model.add(Dense(num_classes, activation='softmax'))
 
-    return model
+def parametric_relu(_x):
+    """Source: https://stackoverflow.com/a/40264459/."""
+    alphas = tf.get_variable('alpha', _x.get_shape()[-1],
+                             initializer=tf.constant_initializer(0.0),
+                             dtype=tf.float32)
+    pos = tf.nn.relu(_x)
+    neg = alphas * (_x - abs(_x)) * 0.5
+
+    return pos + neg
